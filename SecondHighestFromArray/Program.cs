1f6252b938ed4d566b04a8b269f2cf4d1889600a
vishal.Alok <you@example.com>
2021-10-15 03:49:48 +0530

    Add project files.

---
+++ SecondHighestFromArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SecondHighestFromArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] MyArray = new int[] { 5, 8, 11, 34, 23 };
+
+            int largest = int.MinValue;
+            int second = int.MinValue;
+
+            foreach (int i in MyArray)
+            {
+                if (i > largest)
+                {
+                    second = largest;
+                    largest = i;
+                }
+                else if (i > second)
+                    second = i;
+            }
+            System.Console.WriteLine(second);
+        }
+    }
+}
